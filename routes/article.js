acd06a1282a5830fd753088ab085b497fe740955
Danilo Casim Jr. <danilocasim174@gmail.com>
2025-12-21 12:01:02 +0800

    fix: change the folder route to middleware

--- routes/article.js
+++ routes/article.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import articleController from "../controllers/article.js";
-import authenticate from "../middlewares/auth.js";
-import verifyToken from "../middlewares/verifyToken.js";
+import authenticate from "../middleware/auth.js";
+import verifyToken from "../middleware/verifyToken.js";
 
 const articleRouter = Router();
 
