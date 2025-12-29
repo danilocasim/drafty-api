d1f6bc2e52ac916c823b2d729a36d2929e71db45
Danilo Casim Jr. <danilocasim174@gmail.com>
2025-12-29 18:01:36 +0800

    fix: use express.json to make the form data to be in req.body

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ import routes from "./routes/index.js";
 const app = express();
 
 app.use(cors());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/blog/v1/", routes.user);
 app.use("/blog/v1/post", routes.post);
